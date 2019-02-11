81bd35bcaff8b6d059f7d45ab94a9289a38d62ce
Greg <you@example.com>
2019-02-11 13:55:55 +0000

    changed welcome message

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ hi: 'buddy' });
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
