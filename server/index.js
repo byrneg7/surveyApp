3191f3905bf4bc3c33e55b652aefb65b735da624
Greg <you@example.com>
2019-02-12 16:41:23 +0000

    made OAuth flow, handles profile info. Refactored

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
+require('./services/passport');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'buddy' });
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
